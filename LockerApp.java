c0ca47a499760574c07b6b690796b8e4f4d2c657
Arindam8282 <darindam507@gmail.com>
2022-02-07 11:45:39 +0530

    added github link to dev-profile

--- LockerApp.java
+++ LockerApp.java
@@ -7,7 +7,7 @@ class LockerApp{
     String key="";
     Scanner input = new Scanner(System.in);
     Locker locker = new Locker();
-    System.out.println("_______LOCKER APP_______\n   Developer Details\nName : Arindam Dutta\nEmail: darindam507@gmail.com\ncontact : 8282826909\nGitHub : Arindam8282\nSource Code : link");
+    System.out.println("_______LOCKER APP_______\n   Developer Details\nName : Arindam Dutta\nEmail: darindam507@gmail.com\ncontact : 8282826909\nGitHub : Arindam8282\nSource Code : https://github.com/Arindam8282/LockerApp");
 
     while (!key.equals("3")) {
       System.out.print("\n\tLOCKER MENU\n 1.Get All Files\n 2.Operations(Add, Delete, Search)\n 3.Close\n Enter Key - ");
